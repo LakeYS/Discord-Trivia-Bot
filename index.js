d030e6b55eb1992a1931e6bc8a280494735bead0
Lake <LakeYS@users.noreply.github.com>
2017-08-01 19:04:13 -0400

    Added version checker
    Added a version checker that queries this repostory's releases and
    compares the versions.
    
    Ported from 7DTD-Discord
    (https://github.com/LakeYS/7DTD-Discord/commit/4b1fb3ff5507be9d941b9f706d187a2ebcf3115a)

--- index.js
+++ index.js
@@ -1,6 +1,65 @@
 /*jshint esversion: 6 */
 /*jshint evil:true */
 
+const https = require("https");
+const semver = require("semver-compare");
+
+// # Version Check # //
+var options = {
+  host: 'api.github.com',
+  path: '/repos/LakeYS/Discord-Trivia-Bot/releases/latest',
+  method: 'GET',
+  headers: {'user-agent':'Discord-Trivia-Bot'}
+};
+
+var input = "";
+json = "";
+var request = https.request(options, (res) => {
+  res.on('data', (data) => {
+    input = input + data; // Combine the data
+  });
+  res.on('error', (err) => {
+    console.log(err);
+  });
+  res.on('uncaughtException', (err) => {
+    console.log(err);
+  });
+
+  // Note that if there is an error while parsing the JSON data, the bot will crash.
+  res.on('end', function() {
+    if(input !== undefined) {
+      json = JSON.parse(input.toString());
+      if(json.tag_name !== undefined) {
+        release = json.tag_name.replace("v",""); // Mark the release
+
+        // Compare this build's version to the latest release.
+        var releaseRelative = semver(pjson.version, release);
+
+        if(releaseRelative == 1)
+          console.log("********\nNOTICE: You are currently running v" + pjson.version + ". This build is considered unstable.\nCheck here for the latest stable versions of this script:\nhttps://github.com/LakeYS/Discord-Trivia-Bot/releases\n********");
+
+        if(releaseRelative == -1)
+          console.log("********\nNOTICE: You are currently running v" + pjson.version + ". A newer version is available.\nCheck here for the latest version of this script:\nhttps://github.com/LakeYS/Discord-Trivia-Bot/releases\n********");
+        } else {
+          console.log(json);
+          console.log("ERROR: Unable to parse version data.");
+        }
+      }
+    else {
+      console.log(input); // Log the input on error
+      console.log("ERROR: Unable to parse version data.");
+    }
+  });
+});
+
+request.end();
+process.nextTick(() => {
+  request.on('error', (err) => {
+    console.log(err);
+    console.log("ERROR: Unable to query version data.");
+  });
+});
+
 // # Requirements/Init # //
 process.stdin.resume();
 process.stdin.setEncoding('utf8');
