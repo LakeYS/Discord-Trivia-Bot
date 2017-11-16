40d0fe07c0c7c502d22778215c69d5dc64dde379
Lake <LakeYS@users.noreply.github.com>
2017-11-16 16:15:03 -0500

    Auto-restart for run.bat
    This implements a simple auto-restart solution for Windows to roughly
    match the one implemented in run.sh.

--- run.bat
+++ run.bat
@@ -1,3 +1,12 @@
-rem TODO: Auto-restart for Windows systems.
+@ECHO OFF
+:loop
+echo Press N to shut down the bot, otherwise it will automatically start in 5 seconds.
+choice /t 5 /c yn /cs /d y /m "Start bot Y/N?"
+if errorlevel 3 goto :yes
+if errorlevel 2 goto :no
+if errorlevel 1 goto :yes
+:yes
 node index.js
+GOTO :loop
+:no
 pause
