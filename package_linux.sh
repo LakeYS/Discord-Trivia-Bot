1339de3b91622a00a8208ff7dd335db5e5a1d384
Lake <contact@lakeys.net>
2018-01-05 15:47:33 -0500

    Add package scripts

---
+++ package_linux.sh
@@ -0,0 +1,9 @@
+mkdir TriviaBot
+cp config.json TriviaBot
+cp package.json TriviaBot
+cp discord-trivia-func.js TriviaBot
+cp index.js TriviaBot
+cp shard.js TriviaBot
+cp run.sh TriviaBot
+cp profile.png TriviaBot
+cp README.md TriviaBot
---
+++ package_win.sh
@@ -0,0 +1,11 @@
+mkdir TriviaBot
+cp config.json TriviaBot
+cp package.json TriviaBot
+cp discord-trivia-func.js TriviaBot
+cp index.js TriviaBot
+cp shard.js TriviaBot
+cp install.bat TriviaBot
+cp run.bat TriviaBot
+cp profile.png TriviaBot
+cp README.md TriviaBot
+cp run_silent.vbs TriviaBot
