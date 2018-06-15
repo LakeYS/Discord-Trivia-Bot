eca0a948ee7e36245e06f78014d0bf71c7c734e2
Lake Y <contact@lakeys.net>
2018-06-15 13:38:53 -0400

    Add additional typeof check for game
    This is an attempt to prevent an issue with 'trivia stop' where a type error is thrown.

--- triviabot.js
+++ triviabot.js
@@ -443,7 +443,7 @@ function doTriviaGame(id, channel, author, scheduled, category) {
         game[id].timeout = void 0;
         triviaEndGame(id);
       }
-      else if(typeof msg !== "undefined") {
+      else if(typeof msg !== "undefined" && typeof game[id] !== undefined) {
 
         if(game[id].category) {
           // Stat: Rounds played - custom
