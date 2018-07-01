2bb6772bbc9b958140fb4c6eeb29cbf5393cac80
Lake Y <contact@lakeys.net>
2018-07-01 15:58:00 -0400

    Add run_silent.sh to package

--- package_linux.sh
+++ package_linux.sh
@@ -7,6 +7,7 @@ cp triviabot.js TriviaBot
 cp index.js TriviaBot
 cp shard.js TriviaBot
 cp run.sh TriviaBot
+cp run_silent.sh TriviaBot
 cp profile.png TriviaBot
 cp README.md TriviaBot
 cp lib TriviaBot/lib -r
