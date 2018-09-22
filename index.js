11c01799e03942eac9c52a13dbbb6ed0fb595270
Lake <contact@lakeys.net>
2018-09-22 14:46:07 -0400

    Move eval functionality to evalCmds.js
    This allows for the addition of more commands without inflating index.js.

--- index.js
+++ index.js
@@ -230,70 +230,7 @@ manager.on("message", (shard, input) => {
 });
 
 // # Console Functions # //
-function doExit() {
-  manager.broadcastEval("client.destroy(); Trivia.database.destroy();")
-  .then(() => {
-    process.exit();
-  });
-}
-
-var evalCmds = {};
-
-evalCmds.exit = () => {
-  doExit();
-};
-
-evalCmds.stop = () => {
-  doExit();
-};
-
-evalCmds.exportall = () => {
-  console.log("Exporting game for all processes...");
-  manager.broadcastEval("global.Trivia.exportGame();")
-  .catch((err) => {
-    console.error(err);
-  });
-};
-
-evalCmds.importall = () => {
-  console.log("Importing game for all processes...");
-  manager.broadcastEval("global.Trivia.importGame(\"./game.\" + global.client.shard.id + \".json.bak\");")
-  .catch((err) => {
-    console.error(err);
-  });
-};
-
-evalCmds.exportexit = () => {
-  console.log("Exporting game for all processes...");
-  manager.broadcastEval("global.Trivia.exportGame();")
-  .catch((err) => {
-    console.error(err);
-  })
-  .then(() => {
-    doExit();
-  });
-};
-
-evalCmds.exportexit = () => {
-  console.log("Exporting game for all processes...");
-  manager.broadcastEval("global.Trivia.exportGame();")
-  .catch((err) => {
-    console.error(err);
-  })
-  .then(() => {
-    doExit();
-  });
-};
-
-evalCmds.shutdown = () => {
-  manager.broadcastEval("global.Trivia.doMaintenanceShutdown();")
-  .catch((err) => {
-    console.error(err);
-  })
-  .then(() => {
-    console.log("MAINTENANCE SHUTDOWN - All active games cleared. Type \"Exit\" to complete the shutdown.");
-  });
-};
+const evalCmds = require("./lib/evalCmds.js")(manager);
 
 if(config["allow-eval"] === true) {
   process.stdin.on("data", (text) => {
