18c58067df9aedb7614fba82576d0f062b416cd9
Lake <contact@lakeys.net>
2019-01-09 12:22:08 -0500

    Leaderboard score writing array
    Leaderboard now has the ability to append  multiple sections at once. This is used for the monthly/weekly scoring model of the league system.

--- triviabot.js
+++ triviabot.js
@@ -255,7 +255,7 @@ function triviaEndGame(id) {
   }
 
   if(game[id].isLeagueGame) {
-    leaderboard.writeScores(game[id].scores, game[id].guildId, "Weekly");
+    leaderboard.writeScores(game[id].scores, game[id].guildId, ["Monthly", "Weekly"]);
   }
 
   delete game[id];
