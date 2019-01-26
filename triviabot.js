a7a10074e2a180f338a98c0f521cc2def526f494
Lake <contact@lakeys.net>
2019-01-26 14:16:58 -0500

    Trivia stop now accepts a channel as an arg
    "trivia stop" can now stop a game in another channel, for example 'trivia stop #trivia' to stop a game in the #trivia channel. Help command has been updated to reflect the changes.

--- triviabot.js
+++ triviabot.js
@@ -855,23 +855,49 @@ function parseCommand(msg, cmd) {
     doTriviaPing(msg);
   }
 
-  if(cmd === "STOP" || cmd === "CANCEL" || cmd === "ADMIN STOP" || cmd === "ADMIN CANCEL") {
+  if(cmd.startsWith("STOP")) {
+    var stopChannel = msg.channel;
+
     var isAdmin;
     if(((msg.member !== null && msg.member.permissions.has("MANAGE_GUILD")) || msg.channel.type === "dm") && getConfigVal("disable-admin-commands", msg.channel) !== true) {
       isAdmin = true;
+
+      var channelInput = cmd.replace("STOP ","");
+
+      if(channelInput !== "STOP") {
+        var idInput = channelInput.replace("<#","").replace(">","");
+        stopChannel = msg.guild.channels.find((obj) => (obj.id === idInput));
+
+        if(stopChannel === null) {
+          triviaSend(msg.channel, msg.author, `Could not find that channel. Check input and try again. (Example: <#${msg.channel.id}>)`);
+          return;
+        }
+        else if(typeof game[stopChannel.id] === "undefined") {
+          triviaSend(msg.channel, msg.author, "There is no game running in that channel.");
+          return;
+        }
+        else {
+          triviaSend(msg.channel, msg.author, `Stopping game in channel <#${stopChannel.id}>`);
+        }
+      }
     }
 
     if(cmdPlayAdv.advGameExists(id)) {
       cmdPlayAdv.cancelAdvGame(id);
-      triviaSend(msg.channel, void 0, "Game cancelled.");
+      triviaSend(stopChannel, void 0, "Game cancelled.");
+
+      return;
     }
-    else if(typeof game[id] !== "undefined" && game[id].inProgress) {
+
+    if(typeof game[stopChannel.id] !== "undefined" && game[stopChannel.id].inProgress) {
       if(isAdmin) {
-        doTriviaStop(msg.channel);
+        doTriviaStop(stopChannel);
       }
       else {
         triviaSend(msg.channel, void 0, `Trivia games will end automatically if the game is inactive for more than ${getConfigVal("rounds-end-after", msg.channel)-1} round${getConfigVal("rounds-end-after", msg.channel)-1===1?"":"s"}. Only users with the "Manage Server" permission can force-end a game.`);
       }
+
+      return;
     }
   }
 
