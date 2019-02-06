LakeYS/Discord-Trivia-Bot

commit 81b40ff7e316e688070f36aa95b6c7eb8f9e6d21

Add extra breaks to command parser
Command parser now returns in several places to reduce unnessary checks performed after a command is successfully responded to. This is more of a temporary solution until a more efficient and expandable command parser is written.

triviabot.js
@@ -846,6 +846,7 @@ function parseCommand(msg, cmd) {
 
   if(cmd === "PING") {
     doTriviaPing(msg);
+    return;
   }
 
   if(cmd.startsWith("STOP")) {
@@ -871,6 +872,7 @@ function parseCommand(msg, cmd) {
         }
         else {
           Trivia.send(msg.channel, msg.author, `Stopping game in channel <#${stopChannel.id}>`);
+          return;
         }
       }
     }
@@ -921,6 +923,7 @@ function parseCommand(msg, cmd) {
         }
         else {
           Trivia.doGame(msg.channel.id, msg.channel, msg.author, 0, category.id);
+          return;
         }
       })
       .catch((err) => {
@@ -935,15 +938,18 @@ function parseCommand(msg, cmd) {
     else {
       // No category specified, start a normal game. (The database will pick a random category for us)
       Trivia.doGame(msg.channel.id, msg.channel, msg.author, 0);
+      return;
     }
   }
 
   if(typeof commands.leagueParse !== "undefined" && cmd.startsWith("LEAGUE ")) {
     commands.leagueParse(msg.channel.id, msg.channel, msg.author, msg.member, cmd);
+    return;
   }
 
   if(cmd === "CATEGORIES") {
     commands.triviaCategories(msg, Trivia); // TODO: Refactor
+    return;
   }
 }
 

commit 830923467de1c888c5727082091f3d128c38b24c

Fix "trivia stop #channel"
Fixed "trivia stop" not stopping a game in another channel. This was caused by the changes in 81b40ff.

triviabot.js
@@ -901,7 +901,7 @@ function parseCommand(msg, cmd) {
         }
         else {
           Trivia.send(msg.channel, msg.author, `Stopping game in channel <#${stopChannel.id}>`);
-          return;
+          // No return here, need to actually stop the game below.
         }
       }
     }