cc5f94feccf15b1b817abf332b200f549592dd13
Lake <contact@lakeys.net>
2020-01-29 17:59:25 -0500

    Add null exit code warning for shards

--- index.js
+++ index.js
@@ -199,6 +199,11 @@ manager.on("launch", (shard) => {
   // TODO: Rate limit this to prevent API flooding
   shard.on("death", (process) => {
     console.error("Shard " + shard.id + " closed unexpectedly! PID: " + process.pid + "; Exit code: " + process.exitCode + ".");
+
+    if(process.exitCode === null)
+    {
+      console.warn("WARNING: Shard " + shard.id + " exited with NULL error code. This may be a result of a lack of available system memory. Ensure that there is enough memory allocated to continue.");
+    }
   });
 });
 
