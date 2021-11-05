323eaeecea726859c5c182ac3630107b47359a85
Lake <contact@lakeys.net>
2021-11-04 20:16:54 -0400

    Migrate log header to LogManager
    Migrate log heading to the LogManager object and import it into index.js.
    
    Leave the contents of initLogs unchanged apart from moving to the new class.

--- index.js
+++ index.js
@@ -1,58 +1,10 @@
-const pjson = require("./package.json");
+const pkg = require("./package.json");
 const fs = require("fs");
 const { ShardingManager } = require("discord.js");
+const LogManager = require("./lib/log_manager.js");
 
-process.title = `TriviaBot ${pjson.version}`;
-
-function initLogs(config) {
-  // process.stdout.columns returns "undefined" in certain situations
-  var strArray = [ `\x1b[7m TriviaBot Version ${pjson.version}`,
-                   "\x1b[7m Copyright (c) 2018-2021 Lake Y",
-                   "\x1b[7m https://lakeys.net" ];
-
-  var padLen = Math.max(strArray[1].length+1, strArray[0].length+1);
-  // Adjust length of the first line
-  for(var i = 0; i <= strArray.length-1; i++) {
-    strArray[i] = strArray[i].padEnd(padLen, " ") + "\x1b[0m";
-  }
-
-  var strHeader = `${strArray[0]}\n${strArray[1]}\n${strArray[2]}`;
-
-  // Optional logo display
-  if(typeof config !== "undefined" && config["display-ascii-logo"]) {
-    var useSideStr = process.stdout.columns > 61;
-
-    // Use a pattern to properly space the logo.
-    var patt = /^ {3}./mg;
-
-    // See here for an example of how this looks when the application is running:
-    // http://lakeys.net/triviabot/console.png
-    console.log(`\
-                     ########
-                ##################
-             ###      #######     ###
-           ###    ###############   ###
-         ###    ####################  ###
-        ###     #########    ########  ###
-       ###     ########      ########   ###
-      ###       #####       ########     ###
-     ###                  ##########      ### ${useSideStr?strArray[0]:""}
-     ###               ###########        ### ${useSideStr?strArray[1]:""}
-     ###              #########           ### ${useSideStr?strArray[2]:""}
-      ###             ########           ###
-       ###            ######            ###
-        ###            ####            ###
-          ###         ######         ###
-            ###      #######       ###
-              #####    ####    #####
-                   ############
-                      ######\n${useSideStr?"":strHeader}`
-    .replace(patt, ""));
-  }
-  else {
-    console.log(`${strHeader}\n`);
-  }
-}
+var logs = new LogManager(pkg.version);
+process.title = `TriviaBot ${pkg.version}`;
 
 // # Initialize Config Args # //
 var config;
@@ -68,12 +20,12 @@ try {
 }
 catch(err) {
   // Config file broken or missing -- display the initial message and an error
-  initLogs();
+  logs.initLogs({ displayAsciiLogo: config["display-ascii-logo"] });
   console.error("Unable to load config file: " + err.message);
   process.exit();
 }
 
-initLogs(config);
+logs.initLogs({ displayAsciiLogo: config["display-ascii-logo"] });
 
 // # Requirements/Init # //
 const configPrivate = {
@@ -81,7 +33,7 @@ const configPrivate = {
   githubName: "Discord-Trivia-Bot"
 };
 
-require("./lib/init.js")(pjson, config, configPrivate);
+require("./lib/init.js")(pkg, config, configPrivate);
 
 if(config["allow-eval"] === true) {
   process.stdin.resume();
