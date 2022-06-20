820555e8af77bbef8db4f79a0f4cb96045d91676
Lake <contact@lakeys.net>
2022-06-19 22:59:23 -0400

    Replace ESLint config

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  "env": {
+    "es2021": true,
+    "node": true
+  },
+  "extends": [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended"
+  ],
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": {
+    "ecmaVersion": "latest",
+    "sourceType": "module"
+  },
+  "plugins": [
+    "@typescript-eslint"
+  ],
+  "rules": {
+    "linebreak-style": [
+      "error",
+      "windows"
+    ],
+    "quotes": [
+      "error",
+      "double"
+    ],
+    "semi": [
+      "error",
+      "always"
+    ],
+    "no-var-requires": "off",
+    "indent": "off",
+    "no-console": "off",
+    "no-irregular-whitespace": "off",
+  }
+};
