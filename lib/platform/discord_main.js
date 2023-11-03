LakeYS/Discord-Trivia-Bot

commit 9598adb7d974ce4ce460b4a5df1ae1a154596d41

Fix error running games in DM Fix games requiring a member in order to run by passing in a user object (required) to TriviaDiscord.doCommand and denoting the 'member' arg as optional along with several others

lib/platform/discord_main.js
@@ -2,8 +2,14 @@ const {
   ActionRowBuilder,
   ButtonBuilder,
   ButtonStyle,
+  Channel,
   ChannelType,
-  PermissionsBitField
+  PermissionsBitField,
+  Guild,
+  GuildMember,
+  Message,
+  CommandInteractionOptionResolver,
+  User
 } = require("discord.js");
 const entities = require("html-entities").AllHtmlEntities;
 const fs = require("fs");
@@ -557,7 +563,21 @@ class TriviaDiscord extends TriviaInstance {
     }
   }
 
-  doCommand(cmd, reply, replyDirect, channel, member, guild, msg, args) {
+  /**
+   * Invokes a command.
+   * 
+   * @param {string} cmd - The name of the command
+   * @param {Function} reply - Callback for replying to the command within the channel.
+   * @param {Function} replyDirect - Callback for replying to the command as a private or ephemeral message. In slash command mode, this will use an ephemeral message. For text commands, it will be a DM to the user if available.
+   * @param {Channel} channel - The channel we are operating in.
+   * @param {User} user - The user we are handling.
+   * @param {GuildMember} [member] - The guild member if not in a DM.
+   * @param {Guild} [guild] - The guild we are operating in.
+   * @param {Message} [msg] - 
+   * @param {CommandInteractionOptionResolver} [args] - Optional args with the command, such as the category selection for the play command
+   * @returns {void}
+   */
+  doCommand(cmd, reply, replyDirect, channel, user, member, guild, msg, args) {
     const isAdmin = this.checkAdmin(channel, member);
     var game = this.gameHandler.getActiveGame(channel.id);
     var commands = this.commands;
@@ -620,7 +640,7 @@ class TriviaDiscord extends TriviaInstance {
     }
   
     if(cmd.startsWith("CONFIG")) {
-      commands.triviaConfig(cmd, channel, member.user, isAdmin);
+      commands.triviaConfig(cmd, channel, user, isAdmin);
     }
   
     if(cmd.startsWith("RESET")) {
@@ -634,7 +654,7 @@ class TriviaDiscord extends TriviaInstance {
         return;
       }
   
-      commands.triviaPlayAdvanced(reply, replyDirect, void 0, channel.id, channel, member.user, cmd.replace("PLAY ADVANCED",""), undefined, undefined, args);
+      commands.triviaPlayAdvanced(reply, replyDirect, void 0, channel.id, channel, user, cmd.replace("PLAY ADVANCED",""), undefined, undefined, args);
       return;
     }
   
@@ -647,7 +667,7 @@ class TriviaDiscord extends TriviaInstance {
         reply("*(Beware: Some questions from OpenTDB are not designed for hangman-style gameplay)*");
       }
       
-      commands.triviaPlay(reply, replyDirect, channel.id, member.user.id, guild, categoryInput, "hangman");
+      commands.triviaPlay(reply, replyDirect, channel.id, user.id, guild, categoryInput, "hangman");
       this.postStat("commandPlayHangmanCount", 1);
       return;
     }
@@ -668,7 +688,7 @@ class TriviaDiscord extends TriviaInstance {
         mode = "reaction";
       }
 
-      commands.triviaPlay(reply, replyDirect, channel.id, member.user.id, guild, categoryInput, mode);
+      commands.triviaPlay(reply, replyDirect, channel.id, user.id, guild, categoryInput, mode);
       return;
     }
   
@@ -708,6 +728,8 @@ class TriviaDiscord extends TriviaInstance {
   parseSlash(commandName, interaction) {
     // Track this per interaction that we initiate.
     var interactionReplySent = false;
+
+    const isDM = interaction.channel.type === ChannelType.DM;
 
     const replyDirect = (response) => {
       var msg = parseMsg(response);
@@ -760,14 +782,20 @@ class TriviaDiscord extends TriviaInstance {
         }
       };
 
-      const member = interaction.guild.members.me;
-      const perms = interaction.channel.permissionsFor(member);
-
-      const hasChannelPerms = perms.has(
-        PermissionsBitField.Flags.SendMessages &&
-        PermissionsBitField.Flags.EmbedLinks &&
-        PermissionsBitField.Flags.ViewChannel
-      );
+      let hasChannelPerms;
+      
+      if(isDM) {
+        hasChannelPerms = true;
+      } else {
+          const member = interaction.guild.members.me;
+          const perms = interaction.channel.permissionsFor(member);
+    
+          hasChannelPerms = perms.has(
+            PermissionsBitField.Flags.SendMessages &&
+            PermissionsBitField.Flags.EmbedLinks &&
+            PermissionsBitField.Flags.ViewChannel
+          );
+      }
 
       if(!hasChannelPerms) {
         handleError(new Error("Missing Permissions for TriviaBot"));
@@ -803,7 +831,17 @@ class TriviaDiscord extends TriviaInstance {
       }
     };
 
-    this.doCommand(interaction.commandName, reply, replyDirect, interaction.channel, interaction.member, interaction.guild, null, interaction.options);
+    this.doCommand(
+      interaction.commandName,
+      reply,
+      replyDirect,
+      interaction.channel,
+      interaction.user,
+      interaction.member,
+      interaction.guild,
+      null,
+      interaction.options
+    );
   }
 
   // # this.parseText #

commit 1019a58b689f2479cd75ce2e991d4f710dcedec5

Fix missing doCommand arg, type safety
- Fix doCommand missing msg.member resulting in errors introduced in 9598adb and partially fixed in 49b8732
- Null-coalesce to undefined for type consistency

lib/platform/discord_main.js
@@ -990,7 +990,7 @@ class TriviaDiscord extends TriviaInstance {
     // If the string starts with the specified prefix (converted to uppercase)
     if(str.startsWith(prefix)) {
       var cmd = str.replace(prefix, "");
-      this.doCommand(cmd, reply, replyDirect, msg.channel, msg.member, msg.guild, msg);
+      this.doCommand(cmd, reply, replyDirect, msg.channel, msg.author, msg.member ?? undefined, msg.guild ?? undefined, msg);
     }
   }
 